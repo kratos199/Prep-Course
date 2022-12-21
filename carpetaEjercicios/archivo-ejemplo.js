d87d5a5943bd29de57137b612dcfd0afd87fbeee
Jorge <kratos.morales2026@gmail.com>
2022-12-21 00:19:59 -0500

    modificacion2

---
+++ carpetaEjercicios/archivo-ejemplo.js
@@ -0,0 +1,6 @@
+//Una variable está formada por un espacio en el sistema de almacenaje y un nombre simbólico que está asociado a dicho espacio. Ese espacio contiene una cantidad de información conocida o desconocida, es decir un valor.
+//un string es una secuencia de caracteres usado para representar el texto. En JavaScript, un String es uno de los valores primitivos y el objeto String es un envoltorio alrededor de un String primitivo.
+//Una función es un bloque de código que realiza alguna operación. Una función puede definir opcionalmente parámetros de entrada que permiten a los llamadores pasar argumentos a la función. Una función también puede devolver un valor como salida.
+//Un if en programación se utiliza para evaluar una expresión condicional: si se cumple la condición (es verdadera), ejecutará un bloque de código. Si es falsa, es posible ejecutar otras sentencias. Como hemos dicho, la condición es una condición lógica, sólo devolverá true o false , y se ejecutará si su valor es true
+//Un valor booleano representa un valor de verdad; es decir, TRUE o FALSE. Una expresión booleana o predicado puede producir un valor de desconocido (unknown), que está representado por el valor nulo.
+
